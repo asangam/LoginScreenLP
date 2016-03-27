e8df09456bf5e257b0c6010e11b63bea4dc00d22
Sangam Adhikari <san.adh2010@gmail.com>
2016-03-27 12:41:11 +0545

    Sample Login Screen
    Sample login screen for C# application inspired from Android Lollipop.

---
+++ LoginScreenLP/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoginScreenLP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You have logged in succesfully","Admin Panel");
+        }
+    }
+}
